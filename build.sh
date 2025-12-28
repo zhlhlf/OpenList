91173b8cee3a7894d4351df905d32f370e6f4a5e
zhlhlf <zhlhlf@gmail.com>
2025-12-28 11:55:50 +0800

    更改build.sh使用zhlhlf库

--- build.sh
+++ build.sh
@@ -21,14 +21,9 @@ fi
 if [ "$1" = "dev" ]; then
   version="dev"
   webVersion="rolling"
-elif [ "$1" = "beta" ]; then
+else
   version="beta"
   webVersion="rolling"
-else
-  git tag -d beta || true
-  # Always true if there's no tag
-  version=$(git describe --abbrev=0 --tags 2>/dev/null || echo "v0.0.0")
-  webVersion=$(eval "curl -fsSL --max-time 2 $githubAuthArgs \"https://api.github.com/repos/$frontendRepo/releases/latest\"" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 fi
 
 echo "backend version: $version"
